224bc41433fc902a7c46d467b2d048a55c2646e2
Mykhailo Kozlov <Mykhailo_Kozlov@epam.com>
2018-10-09 14:54:57 +0300

    changed generation of assets

--- index.js
+++ index.js
@@ -1,14 +1,22 @@
 class WebpackEntriesChunksResolvePlugin {
     /**
-     * @param {bool} options.generateWithScriptTags define if the output file will contain script tags associated with entry(ies)
-     * @param {string} options.scriptTagPathPrefix define the prefix path of the script tag source attribute.
-     * @param {string} options.fileName resulting filename. Default is entries-dependencies.txt.
+     * @param {bool} options.generateWithScriptTags define if the output file will contain script tags associated with entry(ies). Default is true.
+     * @param {string} options.scriptTagPathPrefix define the prefix path of the script tag source attribute. Default is an empty string.
+     * @param {string} options.fileNameTemplate resulting filename that should contain [entryname] placeholder. Default is [entryname].txt.
      */
     constructor(options) {
         this.options = options;
 
-        if (!options || !optiosn.fileName) {
-            this.options.fileName = 'entries-dependencies.txt';
+        if (!options || !options.fileNameTemplate) {
+            this.options.fileNameTemplate = '[entryname].txt';
+        }
+
+        if (!options) {
+            this.options.generateWithScriptTags = true;
+        }
+
+        if (!options || !options.scriptTagPathPrefix) {
+            this.options.scriptTagPathPrefix = '';
         }
     }
 
@@ -16,46 +24,51 @@ class WebpackEntriesChunksResolvePlugin {
         compiler.hooks.emit.tap(
             'WebpackEntriesChunksResolvePlugin',
             (compilation) => {
-                
-                debugger;
-
-                const dependenciesGraph = {};
 
                 const entriesIterator = compilation.entrypoints.entries();
                 for (let entry = entriesIterator.next(); !entry.done; entry = entriesIterator.next()) {
                     let name = entry.value[0];
                     let chunks = entry.value[1].chunks;
-                    dependenciesGraph[name] = [];
+                    
+                    const entryDependenciesAsset = {
+                        name: this.options.fileNameTemplate.replace('[entryname]', name),
+                        data: ''
+                    };
 
                     chunks.map(x => x.files).forEach(files =>
-                        files.forEach(file => dependenciesGraph[name].push(file))
+                        files.forEach(file => 
+                            entryDependenciesAsset.data += `${this._resolveFile(file)}\n`
+                        )
                     );
 
-                    // for (let chunkIndex = 0; chunkIndex < chunks.length; chunkIndex++) {
-                    //     let files = chunks[chunkIndex].files;
-                    //     for (let fileIndex = 0; fileIndex < files.length; fileIndex++) {
-                    //         dependenciesGraph[name].push(files[fileIndex]);
-                    //     }
-                    // }
-                }
+                    this._addAsset(compilation, entryDependenciesAsset);
 
-                let scriptsPerEntry = "";
-
-                for (const entry in dependenciesGraph) {
-                    scriptsPerEntry += `${entry}\n`;
-                    if (dependenciesGraph.hasOwnProperty(entry)) {
-                        dependenciesGraph[entry].forEach(file => {
-                            scriptsPerEntry += `<script src="./${file}"></script>\n`;
-                        });
-                    }
-                }
-
-                compilation.assets["dependency-graph.txt"] = {
-                    source: () => scriptsPerEntry,
-                    size: () => scriptsPerEntry.length
                 }
             }
         );
+    }
+
+    _resolveFile(file) {
+        if (!this.options.generateWithScriptTags) {
+            return file;
+        }
+
+        return `<script src="${this._resolvePath(file)}"></script>`;
+    }
+
+    _resolvePath(file) {
+        if (!this.options.scriptTagPathPrefix) {
+            return file;
+        }
+
+        return `${this.options.scriptTagPathPrefix}/${file}`;
+    }
+
+    _addAsset(compilation, asset) {
+        compilation.assets[asset.name] = {
+            source: () => asset.data,
+            size: () => asset.data.length
+        };
     }
 }
 
